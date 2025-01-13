1ed32b3f111ec345660280474c704ef1fa9cbbfd
Luminous <lumi9259@gmail.com>
2025-01-13 22:52:53 +0545

    darkmode css changes

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import React from "react";
 import { assets, infoList, toolsData } from "../../../assets/assets";
-
-const About = () => {
+interface NavBarProps {
+  isDarkMode: boolean;
+}
+const About: React.FC<NavBarProps> = ({ isDarkMode }) => {
   return (
     <div
       id="about"
@@ -26,24 +28,30 @@ const About = () => {
             contributing to their success and growth.
           </p>
           <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
-            {infoList.map(({ icon, title, description }, index) => (
+            {infoList.map(({ icon, title, description, iconDark }, index) => (
               <li
                 key={index}
                 className="border-[0.5px] border-gray-400 rounded-xl
-                 p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black"
+                 p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover"
               >
                 <Image
-                  src={icon}
+                  src={isDarkMode ? iconDark : icon}
                   alt={title}
                   className="w-7 mt-3"
                 />
-                <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
-                <p className="text-gray-600 text-sm">{description}</p>
+                <h3 className="my-4 font-semibold text-gray-700 dark:text-white">
+                  {title}
+                </h3>
+                <p className="text-gray-600 text-sm dark:text-white/80">
+                  {description}
+                </p>
               </li>
             ))}
           </ul>
 
-          <h4 className="my-6 text-gray-700 font-Ovo">Tools I use</h4>
+          <h4 className="my-6 text-gray-700 font-Ovo dark:text-white/80">
+            Tools I use
+          </h4>
           <ul className="flex items-center gap-3 sm:gap-5">
             {toolsData.map((tool, index) => (
               <li
