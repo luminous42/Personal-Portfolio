108aa12e29ea4da6925b80546ff02448f7a14dd9
Luminous <lumi9259@gmail.com>
2025-01-15 22:36:40 +0545

    adding motion

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,27 +1,42 @@
 import Image from "next/image";
 import React from "react";
 import { assets } from "../../../assets/assets";
+import { motion } from "motion/react";
 interface NavBarProps {
   isDarkMode: boolean;
 }
 const Header: React.FC<NavBarProps> = ({ isDarkMode }) => {
   return (
     <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
-      <div>
+      <motion.div
+        initial={{
+          scale: 0,
+        }}
+        whileInView={{ scale: 1 }}
+        transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
+      >
         <Image
           src={assets.profile_img}
           alt=""
           className="rounded-full w-32"
         />
-      </div>
-      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
+      </motion.div>
+      <motion.h3
+        initial={{
+          y: -20,
+          opacity: 0,
+        }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.3 }}
+        className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo"
+      >
         Hi! I'm Luminous Suwal
         <Image
           src={assets.hand_icon}
           alt=""
           className="w-6"
         />
-      </h3>
+      </motion.h3>
       <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
         Full Stack Developer based in Kathmandu.
       </h1>
